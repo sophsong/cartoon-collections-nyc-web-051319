8d0bc89323f8a9401724b0d1c13b8ff9cca947fc
Sophia Song <ososanne@gmail.com>
2019-05-07 02:30:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,16 +17,7 @@ end
 
 roll_call_dwarves(dwarves)
 
-# Give the planeteer calls some heart!
-#
-# The summon_captain_planet method should return a new array of values for each
-# of the values in the `planeteer_calls` variable. Each value in the new array
-# should be capitalized and have an exclamation point:
-#
-# earth -> Earth!
-#
-# Use the "map" method (aka the "collection" method):
-# http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-map
+
 
 planeteer_calls = %w[earth wind fire water heart]
 
@@ -37,11 +28,7 @@ end
 
 summon_captain_planet(planeteer_calls)
 
-# The planeteer_calls are supposed to be short, so we want to
-# find out if any of our calls have more than 4 characters.
-#
-# Use the any? method
-# http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-any-3F
+
 
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
@@ -49,25 +36,7 @@ end
 
 long_planeteer_calls(planeteer_calls)
 
-# Help Monterey Jack find the cheese!
 
-# The "find_the_cheese" method should look through an array of strings
-# and find the first string the matches one of the following
-# cheese strings: "cheddar", "gouda", or "camembert"
-#
-# Given an array that looks like: ["shoe", "rope", "gouda"],
-# the value "gouda" should be returned by "find_the_cheese".
-#
-# Given an array that looks like: ["cheddar", "rope", "pie"],
-# the value "cheddar" should be returned by "find_the_cheese".
-#
-# If the array of items contains no cheese, then "find_the_cheese"
-# should return nil.
-
-# Assume that all strings will be lower-case.
-
-# Use the find method
-# http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-find
 
 potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 
